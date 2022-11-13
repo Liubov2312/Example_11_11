4654e289af178d0b2eab442251420cc9c1261d80
Liubov.V.B <griark0611@gmail.com>
2022-11-13 19:19:43 +0300

    "Две задачи с лекции"

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,17 @@
 ﻿Console.WriteLine("2 Hello, World!");
+Console.WriteLine("Week days!");
+string[] Days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+int index = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Today is {Days[index-1]}");
+
+
+
+Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = -a;
+
+while (b <= a)
+{
+    Console.Write(b + ", ");
+    b++;
+}
